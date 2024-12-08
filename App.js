2154896cf84063768877ce19d4a74557d1bcc076
Diego Berrio <dig-mac@MacBook-Air-de-Diego.local>
2024-12-08 12:30:59 -0500

    README.m

--- App.js
+++ App.js
@@ -1,20 +1,105 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useRef } from "react";
+import {
+  StyleSheet,
+  Animated,
+  SafeAreaView,
+  View,
+  Text,
+  TouchableWithoutFeedback,
+} from "react-native";
 
 export default function App() {
+  const animatedValues = {
+    opacity: useRef(new Animated.Value(1)).current,
+    scale: useRef(new Animated.Value(1)).current,
+  };
+
+  const { opacity, scale } = animatedValues;
+
+  const handleAnimated = () => {
+    Animated.sequence([
+      Animated.timing(opacity, {
+        toValue: 0,
+        duration: 1000,
+        useNativeDriver: false,
+      }),
+      Animated.timing(scale, {
+        toValue: 2,
+        duration: 1000,
+        useNativeDriver: false,
+      }),
+      Animated.parallel([
+        Animated.timing(opacity, {
+          toValue: 1,
+          duration: 1000,
+          useNativeDriver: false,
+        }),
+        Animated.timing(scale, {
+          toValue: 1,
+          duration: 1000,
+          useNativeDriver: false,
+        }),
+      ]),
+    ]).start();
+  };
+
+  const animatedStyles = {
+    opacity: opacity,
+    transform: [
+      {
+        scale: scale,
+      },
+    ],
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <SafeAreaView style={{ flex: 0, backgroundColor: "#F29F58" }} />
+
+      <View style={styles.container}>
+        <Text style={styles.title}>
+          Toca el cuadro para ver la animación.
+        </Text>
+        <TouchableWithoutFeedback onPress={handleAnimated}>
+          <Animated.View style={[styles.box, animatedStyles]} />
+        </TouchableWithoutFeedback>
+        <View style={styles.footer}>
+          <Text style={styles.footerText}>@Diegoberrio1601</Text>
+        </View>
+      </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#1B1833",
+    alignItems: "center",
+    justifyContent: "space-evenly",
+  },
+  title: {
+    color:'#fff',
+    fontSize:20,
+    fontWeight:'bold'
+    
+    
+  },
+  box: {
+    width: 150,
+    height: 150,
+    backgroundColor: "#AB4459",
+  },
+  footer: {
+    position: "absolute", 
+    bottom: 20, 
+    left: 0,
+    right: 0,
+    alignItems: "center",
+  },
+  footerText: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight:'bold'
   },
 });
---
+++ README.md
@@ -0,0 +1,26 @@
+# Proyecto de Animación en React Native
+
+Este es un proyecto simple en React Native que muestra cómo crear una animación interactiva utilizando la API `Animated` de React Native. La animación se activa cuando tocas un cuadro en la pantalla, haciendo que cambie su opacidad y tamaño.
+
+## Características
+
+- **Animación interactiva**: Toca el cuadro para ver cómo cambia su opacidad y tamaño.
+- **Texto en pantalla**: Instrucciones visibles en la pantalla.
+- **Fuente personalizada**: Se usa la fuente **Poppins** de Google Fonts.
+
+## Tecnologías utilizadas
+
+- **React Native**
+- **Expo**
+
+
+## Instalación
+
+1. Clona este repositorio:
+
+ ```bash
+   # Usando HTTPS
+   git clone https://github.com/Diegoberrio1601/animatedSquare.git
+
+   # Usando SSH
+   git clone git@github.com:Diegoberrio1601/animatedSquare.git
